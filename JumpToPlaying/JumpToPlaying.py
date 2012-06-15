a6fb15bff042312763ae225a5688013b814bd2bf
Timo Loewe <timoloewe91@googlemail.com>
2012-06-15 13:15:42 +0200

    wrap signal connection to small display toggle with try/except Since Rhythmbox 2.97 there is no longer a small display mode, but we want to keep it for compatibility

--- JumpToPlaying/JumpToPlaying.py
+++ JumpToPlaying/JumpToPlaying.py
@@ -24,9 +24,9 @@ context_menu_key = 'context_menu'
 ui_toolbar_button = """
 <ui>
   <toolbar name="ToolBar">
-    <placeholder name="PluginPlaceholder">
-      <toolitem name="ToolBarJumpToPlaying" action="ViewJumpToPlaying"/>
-    </placeholder>
+	<placeholder name="PluginPlaceholder">
+	  <toolitem name="ToolBarJumpToPlaying" action="ViewJumpToPlaying"/>
+	</placeholder>
   </toolbar>
 </ui>
 
@@ -38,27 +38,27 @@ ui_context_menu = """
 
   <popup name="BrowserSourceViewPopup">
 	 <placeholder name="PluginPlaceholder">
-         <menuitem name="BrowserPopupJumpToPlaying" action="ViewJumpToPlaying"/>
-     </placeholder>
+		 <menuitem name="BrowserPopupJumpToPlaying" action="ViewJumpToPlaying"/>
+	 </placeholder>
   </popup>
 
   <popup name="PlaylistViewPopup">
 	 <placeholder name="PluginPlaceholder">
-         <menuitem name="BrowserPopupJumpToPlaying" action="ViewJumpToPlaying"/>
-     </placeholder>
+		 <menuitem name="BrowserPopupJumpToPlaying" action="ViewJumpToPlaying"/>
+	 </placeholder>
   </popup>
 
 
   <popup name="QueuePlaylistViewPopup">
-     <placeholder name="PluginPlaceholder">
-	     <menuitem name="BrowserPopupJumpToPlaying" action="ViewJumpToPlaying"/>
-     </placeholder>
+	 <placeholder name="PluginPlaceholder">
+		 <menuitem name="BrowserPopupJumpToPlaying" action="ViewJumpToPlaying"/>
+	 </placeholder>
   </popup>
 
   <popup name="PodcastViewPopup">
-     <placeholder name="PluginPlaceholder">
-	     <menuitem name="BrowserPopupJumpToPlaying" action="ViewJumpToPlaying"/>
-     </placeholder>
+	 <placeholder name="PluginPlaceholder">
+		 <menuitem name="BrowserPopupJumpToPlaying" action="ViewJumpToPlaying"/>
+	 </placeholder>
   </popup>
 
 </ui>
@@ -95,10 +95,13 @@ class JumpToPlaying(GObject.GObject, Peas.Activatable):
 		self.update_ui()
 
 		# hide the button in Small Display mode
-		small_display_toggle = uim.get_widget ("/MenuBar/ViewMenu/ViewSmallDisplayMenu")
-		tb_widget = uim.get_widget ("/ToolBar/PluginPlaceholder/ToolBarJumpToPlaying")
-
-		self.tb_conn_id = small_display_toggle.connect ('toggled', self.hide_if_active, tb_widget)
+		# Since Rhythmbox 2.97 there is no longer a SmallDisplayMode, but for now we keep it for compatibility 
+		try:
+				small_display_toggle = uim.get_widget ("/MenuBar/ViewMenu/ViewSmallDisplayMenu")
+				tb_widget = uim.get_widget ("/ToolBar/PluginPlaceholder/ToolBarJumpToPlaying")
+				self.tb_conn_id = small_display_toggle.connect ('toggled', self.hide_if_active, tb_widget)
+		except:
+			pass
 
 		# start hidden if in small_display
 		is_small = gconf.client_get_default().get_bool("/apps/rhythmbox/ui/small_display")
@@ -116,10 +119,11 @@ class JumpToPlaying(GObject.GObject, Peas.Activatable):
 		if (self.ui_cm != None):
 			uim.remove_ui(self.ui_cm)
 			del self.ui_cm
-
-		small_display_toggle = uim.get_widget ("/MenuBar/ViewMenu/ViewSmallDisplayMenu")
-		small_display_toggle.disconnect (self.tb_conn_id)
-
+		try:
+			small_display_toggle = uim.get_widget ("/MenuBar/ViewMenu/ViewSmallDisplayMenu")
+			small_display_toggle.disconnect (self.tb_conn_id)
+		except:
+			pass
 		uim.ensure_update()
 
 
